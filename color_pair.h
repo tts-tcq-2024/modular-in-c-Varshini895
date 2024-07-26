beefa952e4105bfac8b66ee96b35c39aa71f7dee
Varshini895 <varshinireddynallamilli@gmail.com>
2024-07-26 15:38:13 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -14,6 +14,8 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
