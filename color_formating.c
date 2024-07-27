22d3f5349bc8d779c358eb7fa2ceae910f0bf942
Varshini895 <varshinireddynallamilli@gmail.com>
2024-07-27 10:44:55 +0530

    Create color_formating.c

---
+++ color_formating.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "color_pair.h"
+#include "color_utils.h"
+
+void PrintColorReferenceManual() {
+    printf("Color Reference Manual:\n");
+    for (int major = 0; major < numberOfMajorColors; major++) {
+        for (int minor = 0; minor < numberOfMinorColors; minor++) {
+            ColorPair colorPair = { (enum MajorColor)major, (enum MinorColor)minor };
+            int pairNumber = GetPairNumberFromColor(&colorPair);
+            char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+            ColorPairToString(&colorPair, colorPairNames);
+            printf("Pair Number: %d, Colors: %s\n", pairNumber, colorPairNames);
+        }
+    }
+}
